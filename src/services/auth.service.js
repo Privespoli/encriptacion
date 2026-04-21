2d67b1e4078ae2da0dd694e8a2dee24c9d82a928
Priscila Nunes <priscilavespoli92@gmail.com>
2026-04-21 16:46:17 +0200

    a ver...

--- src/services/auth.service.js
+++ src/services/auth.service.js
@@ -1,5 +1,6 @@
 import userModel from '../models/user.model.js';
 import bcrypt from 'bcrypt'
+import { createToken } from './token.service.js';
 
 const saltRounds = 10
 
@@ -31,12 +32,51 @@ export async function registerService(userData) {
 }
 
 export async function loginService(userData) {
+    try {
+        const { email, password } = userData;
+        const user = await userModel();
+        const usuario = await user.findOne({ email })
+        // si el usuario no existe entonces retorna el 404
+        if (!usuario) return { status: 401, message: "Usuario o clave incorrecto" }
+
+
+        const compare = await bcrypt.compare(password, usuario.password)
+        // bcrypt compare devuelve true o false si la clave es correcta o no, si no es correcta devolvemos en la api un 404 con un mensaje ambiguo
+        if (!compare) return { status: 401, message: "Usuario o clave incorrecto" }
+        const userInfo = {
+            nombre: usuario.nombre,
+            apellido: usuario.apellido,
+            correo: usuario.email
+        }
+        return {
+            status: 200,
+            message: {
+                usuario,
+                token: createToken(userInfo)
+            }
+
+        };
+    } catch (e) {
+        return {
+            status: 400,
+            message: e.message
+        }
+    }
+}
+
+export async function userInfoService(userData) {
     const { email, password } = userData;
     const user = await userModel();
-    const usuario=await user.findOne({email})
+    const usuario = await user.findOne({ email })
+    if (!usuario) return { status: 404, message: "Usuario no encontrado" }
 
+    const userInfo = {
+        nombre: usuario.nombre,
+        apellido: usuario.apellido,
+        correo: usuario.email
+    }
     return {
-        status: 200,
-        message: usuario
-    };
+        status:200,
+        message: userInfo
+    }
 }
---
+++ src/services/token.service.js
@@ -0,0 +1,5 @@
+import jwt from 'jsonwebtoken'
+export function createToken(userInfo) {
+ const token=jwt.sign(userInfo, process.env.tokenKey)
+    return token
+}
