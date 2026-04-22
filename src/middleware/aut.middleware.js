2c32f4e20d91950d79da8921d35d16c04df659b6
Priscila Nunes <priscilavespoli92@gmail.com>
2026-04-22 11:55:15 +0200

    estudiar y ver se está ok

--- src/middleware/aut.middleware.js
+++
@@ -1,8 +0,0 @@
-export function authMiddleware(req, res, next) {
-    console.log(req.headers.authorization)
-    const token=req.headers.authorization
-    if(!token)res.status(401).send("Token invalido")
-    console.log("ahora si pasa al controlador")
-    // res.send("esto no va a pasar del middleware")
-    next();
-}
